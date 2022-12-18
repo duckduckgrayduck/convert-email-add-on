83f17f6be196ce5ed23a658624897566f4e3d690
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2022-12-17 20:24:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,18 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This is an DocumentCloud Add-On that,
+when given a public Google Drive or Dropbox link containing EML/MSG files,
+will convert them to PDFs and upload them to DocumentCloud
 """
-
 from documentcloud.addon import AddOn
-
-
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class ConvertEmail(AddOn):
+    """DocumentCloud Add-On that converts EML/MSG files to PDFs and uploads them to DocumentCloud"""
 
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
+        #name = self.data.get("name", "world")
 
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
-        for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+        self.set_message("Retrieving EML/MSG files...")
+        self.set_message("Converting EML/MSG files to PDFs...")
+        self.set_message("Uploading converted files to DocumentCloud...")
 
 
 if __name__ == "__main__":
