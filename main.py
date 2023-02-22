530fe6e234f88667778701c9be8ffbc081c4f73d
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-02-22 10:04:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ class ConvertEmail(AddOn):
         """Uses a java program to convert EML/MSG files to PDFs
         extracts attachments if selected"""
         if self.extract_attachments:
-            bash_cmd = f"java -jar email.jar -a -q {file_path}; mv ./out/EMLs/*attachments* attach;"
+            bash_cmd = f"java -jar email.jar -a {file_path}; mv ./out/EMLs/*attachments* attach;"
         else:
-            bash_cmd = f"java -jar email.jar -q {file_path}"
+            bash_cmd = f"java -jar email.jar {file_path}"
         subprocess.call(bash_cmd, shell=True)
 
     def main(self):
