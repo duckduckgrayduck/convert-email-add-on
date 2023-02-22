e52392bdba5cd92a5822099ea8a98303b2175e0a
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-02-22 09:48:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ class ConvertEmail(AddOn):
         for current_path, folders, files in os.walk("./out/"):
             for file_name in files:
                 file_name = os.path.join(current_path, file_name)
-                shutil.move(file_name, file_name.replace(' ', '').replace('(', '').replace(')', ''))
+                clean_file_name = shutil.move(file_name, file_name.replace(' ', '').replace('(', '').replace(')', ''))
                 self.set_message("Attempting to convert EML/MSG files to PDFs...")
-                abs_path = os.path.abspath(file_name)
+                abs_path = os.path.abspath(clean_file_name)
                 print(abs_path)
                 try:
                     self.eml_to_pdf(abs_path)
