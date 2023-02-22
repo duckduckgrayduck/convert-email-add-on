a01275fe7a30089087e6a6b68ace91c841f15cbd
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-02-21 20:43:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ class ConvertEmail(AddOn):
         os.makedirs(os.path.dirname("./out/"), exist_ok=True)
         os.makedirs(os.path.dirname("./attach/"), exist_ok=True)
         downloaded = grab(url, "./out/")
+        os.chdir('./out/')
+        subprocess.call('for file in *; do mv "$file" $(echo "$file" | sed -e 's/[^A-Za-z0-9._-]/_/g'); done &')
+        os.chdir('..')
 
     def eml_to_pdf(self, file_path):
         """Uses a java program to convert EML/MSG files to PDFs
         extracts attachments if selected"""
-        os.chdir('./out/')
-        subprocess.call('for file in *; do mv "$file" $(echo "$file" | sed -e 's/[^A-Za-z0-9._-]/_/g'); done &')
-        os.chdir('..')
         if self.extract_attachments:
             bash_cmd = f"java -jar email.jar -a -q {file_path}; mv ./out/EMLs/*attachments* attach;"
         else:
